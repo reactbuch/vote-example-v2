b98f61f016e7f62479b466b24034f0a5d7743567
stefc <Stefan.Boether@txs.de>
2022-10-11 08:21:40 +0200

    feat: Server kann jetzt per 'HTTPS=true' optional mit Zertifikat betrieben werden

--- voteapp-server/src/server.js
+++ voteapp-server/src/server.js
@@ -1,15 +1,10 @@
 const Database = require("./db/InMemoryVoteDatabase");
 const createVoteApp = require("./app");
-
-const https = require('https');
-const fs = require('fs');
+const https = require("https");
+const fs = require("fs");
 
 const port = process.env.SERVER_PORT || 3000;
-
-const privateKey  = fs.readFileSync('.cert/server.key', 'utf8');
-const certificate = fs.readFileSync('.cert/server.crt', 'utf8');
-
-const credentials = {key: privateKey, cert: certificate};
+const useHttps = process.env.HTTPS === "true";
 
 Database.create((err, database) => {
   if (err) {
@@ -17,9 +12,23 @@ Database.create((err, database) => {
   }
 
   const app = createVoteApp(database);
-  
-  https.createServer(credentials, app).listen(port, () => {
-    console.log(`  📞    Votes API Server listening on port ${port}`);
-  });
 
+  if (useHttps) {
+    const privateKey = fs.readFileSync(".cert/server.key", "utf8");
+    const certificate = fs.readFileSync(".cert/server.crt", "utf8");
+
+    const credentials = { key: privateKey, cert: certificate };
+
+    https.createServer(credentials, app).listen(port, () => {
+      console.log(
+        `  📞    Votes API Server listening on https://localhost:${port}/api`
+      );
+    });
+  } else {
+    app.listen(port, () => {
+      console.log(
+        `  📞    Votes API Server listening on http://localhost:${port}/api`
+      );
+    });
+  }
 });
